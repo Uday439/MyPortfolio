706a26cd8975e84474692bd0d29816f2aa53e7ec
Uday439 <udaykiran6736@gmail.com>
2025-09-18 12:45:00 +0530

    resume

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ArrowDown, FileText, Github, Linkedin, Twitter } from 'lucide-react';
 import Uday from '../Uday.jpg';
-import Resume from '../UDAYKIRAN PENIKALAPATI.pdf'
+import Resume from '../Udaykiran-Soft-Dev.pdf'
 
 const Hero: React.FC = () => {
   return (
